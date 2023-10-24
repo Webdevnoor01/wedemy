5fb64e8112d7dfb9469ba37a1767d502ca3fdb87
Abdun Noor Faruki Biswas <andunnoorfarukibiswas6761@gmail.com>
2023-10-24 12:32:46 +0530

    feat:database(redis) connection, configure cloudinary,  and handle error

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,23 @@
 const mongoose = require("mongoose")
+const { createClient } = require("redis")
 class InitiateDB {
     connect(uri) {
         return mongoose.connect(uri)
+    }
+
+    connectRedis(url){
+        const clint = createClient({
+            url
+        })
+        clint.on("error", (err) => {
+            throw new Error(err)
+        })
+
+        clint.connect().then((param) => {
+            console.log("redis database connected successfully")
+        }).catch((err) => {
+            console.log("failed to connect redis")
+        })
     }
 }
 
